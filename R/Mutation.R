a401eb1f0b69dd9b7cb5ac4279db2f502a11b3cd
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2017-12-04 14:21:58 -0800

    format

--- R/Mutation.R
+++ R/Mutation.R
@@ -1,10 +1,10 @@
 # Set the initial mutation rate = 0.01
 mutation_rate <- 0.01
 
-Mutation <- function(input,mutation_rate){
+Mutation <- function(input, mutation_rate){
   # Generate a copy of the input
   after_mutation <- input
-  
+
   # The logic of this for loop:
   # To make a value change based on a value, I will do following simulation:
   # Generate a random number from Unif(0,1)
@@ -12,10 +12,10 @@ Mutation <- function(input,mutation_rate){
   # If the random number is smaller than the mutation number
   # The corresponding gene will mutate
   # Otherwise, the value will stay the same
-  for(i in length(input)){
+  for (i in length(input)) {
     u <- runif(1)
-    if(u <= mutation_rate){
-      after_mutation[i] <- abs(after_mutation[i]-1)
+    if (u <= mutation_rate) {
+      after_mutation[i] <- abs(after_mutation[i] - 1)
     }
   }
 
@@ -24,5 +24,5 @@ Mutation <- function(input,mutation_rate){
 
 
 # test
-m <- matrix(rep(c(0,1),30000),ncol = 30)
-system.time(Mutation(m,0.01))
+m <- matrix(rep(c(0,1), 30000), ncol = 30)
+system.time(Mutation(m, 0.01))
