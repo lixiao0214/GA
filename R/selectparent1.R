5b5d4fcfd1ee0696cbeab083db5ff6125cceb727
Lei Zhang <lei_zhang@berkeley.edu>
2017-12-10 02:50:42 +0800

    rankbased fitness function

--- R/selectparent1.R
+++ R/selectparent1.R
@@ -32,8 +32,8 @@
 ###and to select the other parent completely at random
 twopropselection <- function(originalparents, couplenum, fitness){
   #' Method : twopropselection
-  #' Select parents independently with probability proportional to fitness
-  #'
+  #' select one parent with probability proportional to fitness(or fitnessrank)
+  #'and to select the other parent completely at random
   #' Returns the selected parents ~ list
   #' @author Lei Zhang
   #' @param originalparents : a casenum * variablenum matrix
@@ -46,7 +46,6 @@ twopropselection <- function(originalparents, couplenum, fitness){
   stopifnot(is.vector(fitness))
 
 
-  fitness <- exp(fitness)/sum(exp(fitness))
   firstparent <- originalparents[sample(1:nrow(originalparents), size = couplenum, replace = TRUE,                                       prob = fitness),]
   secondparent <- originalparents[sample(1:nrow(originalparents), size = couplenum, replace = TRUE),]
   #transform the each row of the matrix into a list
@@ -59,14 +58,10 @@ twopropselection <- function(originalparents, couplenum, fitness){
 
 
 
-
-
-###method two:select each parent independently with probability proportional to fitness
 onepropselection <- function(originalparents, couplenum, fitness){
   #' Method : onepropselection
   #' Select one parent with probability proportional to fitness and to select
   #' the other parent completely at random
-  #'
   #' Returns the selected parents ~ list
   #' @author Lei Zhang
   #' @param originalparents : a casenum * variablenum matrix
@@ -78,9 +73,6 @@ onepropselection <- function(originalparents, couplenum, fitness){
   stopifnot(is.numeric(couplenum))
   stopifnot(is.vector(fitness))
 
-
-
-  fitness <- exp(fitness)/sum(exp(fitness))
   firstparent <- originalparents[sample(1:nrow(originalparents), size = couplenum, replace = TRUE, prob = fitness),]
   secondparent <- originalparents[sample(1:nrow(originalparents), size = couplenum, replace = TRUE, prob = fitness),]
   #transform the each row of the matrix into a list
@@ -91,13 +83,6 @@ onepropselection <- function(originalparents, couplenum, fitness){
   return(newcouple)
 }
 
-
-###method three:tournament selection
-###the set of parents in generation t is randomly partitioned into k disjoint
-###subsets of equal size(perhaps with a few remaining parents temporarily ignored)
-###The best individual in each group is chosen as a parent.
-###Additional random partitionings are carried out until sufficient parents have
-###been generated.Parents are then paired randomly for breeding.
 tournament <- function(originalparents, couplenum, subsetnum, fitness){
   #' Method : tournament selection
   #' the set of parents in generation t is randomly partitioned into k disjoint
@@ -105,11 +90,11 @@ tournament <- function(originalparents, couplenum, subsetnum, fitness){
   #' The best individual in each group is chosen as a parent.
   #' Additional random partitionings are carried out until sufficient parents have
   #' been generated.Parents are then paired randomly for breeding.
-  #'
   #' Returns the selected parents ~ list
   #' @author Lei Zhang
   #' @param originalparents : a casenum * variablenum matrix
   #' @param couplenum : number of couples selected
+  #' @param subsetnum : partition the set of chromosomes in generation t into "subsetnum" disjoint subsets
   #' @param fitness: goodness of fit ~ a casenum * 1 vector , default is AICvalue
   #' @example tournament(originalparents = firstgeneration, couplenum = 5, fitness = AICvalue, subsetnum = 5 )
 
@@ -118,7 +103,6 @@ tournament <- function(originalparents, couplenum, subsetnum, fitness){
   stopifnot(is.vector(fitness))
   stopifnot(is.numeric(subsetnum))
 
-  fitness <- exp(fitness)/sum(exp(fitness))
   partitiontime <- ceiling(couplenum*2/subsetnum)
   parentspool <- matrix(0, partitiontime*subsetnum, ncol(originalparents))
   getsubpars <- function(originalparents,subsetnum){
@@ -148,23 +132,44 @@ tournament <- function(originalparents, couplenum, subsetnum, fitness){
   return(newcouple)
 }
 
+###calculate the fitness based on rank
+getrankfitness <- function(fitness, reverse = TRUE){
+  #' calculate the fitness based on the objective function's rank
+  #' @author Lei Zhang
+  #' @param fitness: goodness of fit ~ a casenum * 1 vector , default is AICvalue
+  #' @param reverse: whether we need to reverse the order when we rank the values in fitness, depends on whether the higher the value in the "fitness" vector , the better the fitness
+  #' @example getrankfitness(fitness = AICvalue, reverse = TRUE); getrankfitness(fitness = Rsquare, reverse = FALSE)
+
+  stopifnot(is.vector(fitness))
+  stopifnot(is.logical(reverse))
+
+  rank <- rank( ((-1)*reverse + 1 *(1 - reverse)) * fitness )
+  rankfit <- 2 * rank/(length(fitness) * (length(fitness)+1))
+  return(rankfit)
+}
 
 
 ###Select parents based on the parameters
 selectparents <- function(originalparents, couplenum, method, fitness, subsetnum = NULL ){
-  #originalparents is a casenum * variablenum matrix
-  #fitness is a casenum * 1 vector , usually it's AICvalue
-  #method is the method to select the parents
+  #' In this selection mechanism, parents are chosen to produce offspring in the next stage.
+  #' Three different methods are provided: "twopropselection", "onepropselection" and "tournament"
+  #' @author Lei Zhang
+  #' @param originalparents is a casenum * variablenum matrix
+  #' @param couplenum: number of couples selected
+  #' @param method: the method to select the parents, "twopropselection", "onepropselection" or "tournament"
+  #' @param fitness: goodness of fit ~ a casenum * 1 vector , default is AICvalue
+  #' @param subsetnum : In method "tournament", we partition the set of chromosomes in generation t into "subsetnum" disjoint subsets
+  #' @example selectparents(originalparents = x, couplenum = 10, method = "tournament", fitness=AICvalue, subsetnum = 5 )
 
-  #fitness <- getfitness(originalparents, fitfun = fitfun)
+  stopifnot(is.matrix(originalparents))
+  stopifnot(is.numeric(couplenum))
+  stopifnot(is.character(method))
+  stopifnot(is.vector(fitness))
+
+  fitness <- getrankfitness(fitness = fitness)
   switch(method,
          twopropselection = twopropselection(originalparents, couplenum, fitness),
          onepropselection = onepropselection(originalparents, couplenum, fitness),
          tournament = tournament(originalparents, couplenum, subsetnum, fitness))
 }
-
-
-
-
-
 
