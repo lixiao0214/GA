b498921d62719f6d663009765ab43b04d327a2ac
许金徽 <xujinhui@calvisitor-10-105-177-216.calvisitor.1918.berkeley.edu>
2017-12-13 13:53:42 -0800

    modify modelling.R

--- R/modelling.R
+++ R/modelling.R
@@ -20,13 +20,20 @@ check_input <- function(input) {
   lapply(input, stopifnot(is.data.frame))
 }
 
+####get dataset and target_names, output Y_variable(one column), X_variable(feature matrix)
+split_data<-function(dataset, target_name){
+  y_variable<-dataset[target_name]
+  x_variable<-setdiff(dataset,y_variable)
+  return(list(y_variable,x_variable))
+}
+
 # Unit function
-get_goodness_of_fit <- function(df, regression_target, criterion = "AIC") {
+get_goodness_of_fit <- function(df, regression_target, x_names,criterion = "AIC") {
   #' Goodness of Fit
   #'
   #' Returns the goodness-of-fit for regression_target ~ df
   #'
-  #' @author Louis Rémus
+  #' @author Louis Rémus, Jinhui Xu
   #' @param df data.frame: regression_target and covariates
   #' @param regression_target character: column of df regressed on other covariates in df
   #' @param criterion character: goodness of fit criterion
@@ -41,15 +48,8 @@ get_goodness_of_fit <- function(df, regression_target, criterion = "AIC") {
   # Get the function
   criterion_function <- match.fun(criterion)
 
-  # list of covariates
-  col_names <- names(df)
-  if (all(col_names != regression_target)) {
-    stop('regression_target not in regression data.frame')
-  }
-  col_names <- col_names[col_names != regression_target]
-
   # Build the formula
-  formula <- as.formula(paste(regression_target, "~", paste(col_names, collapse = " + ")))
+  formula <- as.formula(paste(regression_target, "~", paste(x_names, collapse = " + ")))
   lm_fit <- lm(formula = formula, data = df)
 
   goodness_of_fit <- criterion_function(lm_fit)
@@ -61,7 +61,7 @@ compute_population_goodness_of_fit <- function(data, population, regression_targ
   #' Compute Population Goodness of Fit
   #'
   #' Returns the goodness of fit for the given population
-  #' @author Louis Rémus
+  #' @author Louis Rémus, Jinhui Xu
   #' @param data data.frame: regression_target and covariates
   #' @param population data.frame: selected column per element of the population
   #' @param regression_target character: column of df regressed on other covariates in df
@@ -71,7 +71,6 @@ compute_population_goodness_of_fit <- function(data, population, regression_targ
   #' @return Returns the goodness of fit for the given population
 
   stopifnot(is.data.frame(data))
-  stopifnot(is.data.frame(population))
   stopifnot(is.character(regression_target))
   stopifnot(is.character(criterion))
 
@@ -80,32 +79,26 @@ compute_population_goodness_of_fit <- function(data, population, regression_targ
   # Make sure there is more than zeros column
   stopifnot(dim(data)[2] > 0)
 
-  col_names <- names(population)
-  # Cast columns type to numeric
-  population = as.data.frame(sapply(population, as.numeric))
-  names(population) <- col_names
-  stopifnot(is.data.frame(population))
 
   # Compute goodness of fit for each element of the population
   if (verbose) print('Computing goodness of fit for population')
 
   goodness_of_fit_values <- sapply(1:nrow(population), function(i){
     if (verbose) print(paste("Processing element:", i))
-    selected_columns <- names(population)[which(population[i, ] == 0)]
-    if (length(selected_columns) == dim(population)[2]) {
-      browser()
-      # We have only one column
-      stop("Zero column in regression data.frame")
-    }
-    selected_columns <- c(selected_columns, regression_target)
-    subset_data <- main_dataset[, selected_columns]
-    return(get_goodness_of_fit(df = subset_data, regression_target = regression_target, criterion = criterion))
+    if(sum(population[i,])==0) return(100000)
+    selected_columns <- as.logical(population[i, ])
+
+    y_variable<-split_data(data, regression_target)[[1]]
+    x_variable<-split_data(data, regression_target)[[2]]
+    x_names<-names(x_variable[ selected_columns])
+    subset_data<-cbind(y_variable,x_variable[selected_columns])
+    return(get_goodness_of_fit(df = subset_data, regression_target = regression_target, x_names, criterion = criterion))
   })
-  population['goodness_of_fit'] <- goodness_of_fit_values
+  population<-cbind(population,goodness_of_fit_values)
   return(population)
 }
 
-generate_toy_dataset <- function(n_cols = 7, n_rows = 100, n_population = 100, regression_target = "V1") {
+generate_toy_dataset <- function(n_cols = 7, n_rows = 100, n_population = 50, regression_target = "V1") {
   #' Generate a toy dataset
   #'
   #' Returns a toy dataset in the appropriate format
@@ -142,23 +135,12 @@ generate_toy_dataset <- function(n_cols = 7, n_rows = 100, n_population = 100, r
 }
 
 
-# Testing
-# Parameters
-regression_target <- "V1"
-n_cols = 8
-n_rows = 100
-n_population = 300
 
-# Generate init dataset
-toy_datasets <- generate_toy_dataset(n_cols = n_cols,
-                                     n_rows = n_rows,
-                                     n_population = n_population,
-                                     regression_target = regression_target)
-main_dataset <- toy_datasets$main_dataset
-population <- toy_datasets$population
-
-
+####test
+#####generate dataset and population
+main_dataset <- as.data.frame(matrix(runif(n = 100 * 8), nrow = 100, ncol = 8))
+population <- matrix(rbinom(n = 1000 * 7, prob = 0.5, size = 1), ncol = 7)
 population_new <- compute_population_goodness_of_fit(data = main_dataset,
                                                     population = population,
-                                                    regression_target = "V1")
+                                                    "V1")
 
