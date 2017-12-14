7e409630593e38587dd171157fa8d98665c2b79b
许金徽 <xujinhui@calvisitor-10-105-177-28.calvisitor.1918.berkeley.edu>
2017-12-14 15:37:47 -0800

    1

--- tests/testthat/test_select.R
+++ tests/testthat/test_select.R
@@ -1,11 +1,12 @@
 library(testthat)
+result<-select(data=mtcars,method = "onepropselection",p=2,
+       mutation_rate = 0.01,regression_target = "mpg",
+       scheme = "re-rank",max_iter = 500)
 
-test_that("test crossover_p_splt works",
-          expect_true(
-            is.list(select(data=mtcars,method = "onepropselection",p=2,
-                           mutation_rate = 0.01,regression_target = "mpg",
-                           scheme = "re-rank",max_iter = 500))
-          )
+test_that("test select.R works",{
+          expect_true(is.list(result))
+          expect_true(length(result[[2]]==4))
+}
 )
 
 
