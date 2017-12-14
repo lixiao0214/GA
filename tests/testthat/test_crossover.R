cddc809f7dd74a9879c20a35bc200e48cb2474a9
许金徽 <xujinhui@xujinhuideMBP.attlocal.net>
2017-12-14 00:11:44 -0800

    test for crossover

---
+++ tests/testthat/test_crossover.R
@@ -0,0 +1,10 @@
+library(testthat)
+parents<-list()
+parents[[1]]<-rbind(rbinom(n = 9, size = 1, prob = 0.5),
+                    rbinom(n = 9, size = 1, prob = 0.5))
+
+test_that("test crossover_p_splt works",
+          expect_true(
+            is.matrix(crossover_p_split(parents,p=2))
+            )
+
