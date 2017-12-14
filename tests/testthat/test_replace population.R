11cc0d305a980fd955b97cafe264016c67021c41
许金徽 <xujinhui@calvisitor-10-105-177-28.calvisitor.1918.berkeley.edu>
2017-12-14 13:58:08 -0800

    1

--- tests/testthat/test_replace population.R
+++ tests/testthat/test_replace population.R
@@ -1,5 +1,23 @@
 library(testthat)
+generate_toy_dataset <- function(n_cols = 7, n_rows = 100, n_population = 50, regression_target = "V1") {
 
+  stopifnot(is.character(regression_target))
+
+  main_dataset <- as.data.frame(matrix(runif(n = n_rows * n_cols), nrow = n_rows, ncol = n_cols))
+
+  population <- as.data.frame(matrix(rbinom(n = n_population * n_cols, prob = .5, size = 1), ncol = n_cols))
+
+  if (any(rowSums(population) <= 1)) {
+    # list of covariates
+    col_names <- names(population)
+    col_names <- col_names[col_names != regression_target]
+    # Put a one in the first covariate
+    population[, col_names][rowSums(population[, col_names]) == 0, 1] = 1
+  }
+
+  return(list(population = population[, col_names],
+              main_dataset = main_dataset))
+}
 # Generate random dataset
 regression_target <- "V1"
 toy_datasets <- generate_toy_dataset(regression_target = regression_target)
