83ea52c643534b9b6d1f821add28334ef32c5e80
许金徽 <xujinhui@calvisitor-10-105-177-28.calvisitor.1918.berkeley.edu>
2017-12-14 13:51:57 -0800

    1

---
+++ cross_p_split.R
@@ -0,0 +1,44 @@
+crossover_p_split<-function(parents,p){
+  new_generation<-c()
+  ###calculate the length of chromosomes C, and the numebr of pairs of parents P
+  C<-length(parents[[1]][1,]);P<-length(parents)
+  
+  for(i in 1:P){
+    
+    ###store offsprings
+    offspring_1<-c();offspring_2<-c()
+    
+    ###store parents in two vector
+    parents_1<-parents[[i]][1,];parents_2<-parents[[i]][2,]
+    
+    ###randomly yield index of point to crossover
+    split_index<-sort(sample(2:C,p))
+    split_index<-c(1,split_index,(C+1))
+    ###split parents' chromosomes into p+1 pieces, and store in two lists
+    pieces_1<-list();pieces_2<-list()
+    for(j in 1:(p+1)){
+      pieces_1[[j]]<-parents_1[split_index[j]:(split_index[j+1]-1)]
+      pieces_2[[j]]<-parents_2[split_index[j]:(split_index[j+1]-1)]
+    }
+    
+    ###choose a half of pieces from each parent and get offsprings
+    chosen_pieces_1<-sort(sample(1:(p+1),round(p/2)+1))
+    
+    for(j in 1:(p+1)){
+      
+      ###judge whose jth pieces genes should be inherited  
+      if(sum(chosen_pieces_1==j)==1) {
+        offspring_1<-c(offspring_1,pieces_1[[j]]);offspring_2<-c(offspring_2,pieces_2[[j]])
+      }
+      else{
+        offspring_1<-c(offspring_1,pieces_2[[j]]);offspring_2<-c(offspring_2,pieces_1[[j]])
+      }
+    }
+    
+    ###crossover and get two offsprings
+    new_generation<-rbind(new_generation,offspring_1,offspring_2)
+    
+  }
+  row.names(new_generation)<-c(1:(2*P))
+  return(new_generation)
+}
--- tests/testthat/test_crossover.R
+++ tests/testthat/test_crossover.R
@@ -1,4 +1,4 @@
-library(testthat)
+
 parents<-list()
 parents[[1]]<-rbind(rbinom(n = 9, size = 1, prob = 0.5),
                     rbinom(n = 9, size = 1, prob = 0.5))
@@ -6,5 +6,5 @@ parents[[1]]<-rbind(rbinom(n = 9, size = 1, prob = 0.5),
 test_that("test crossover_p_splt works",
           expect_true(
             is.matrix(crossover_p_split(parents,p=2))
-            )
+            ))
 
